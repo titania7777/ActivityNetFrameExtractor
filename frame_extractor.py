4765c1f31bb0c45e6301f1fadbbf56fe535a488a
Hyeok Yoon <titania7777@gmail.com>
2020-12-22 01:22:05 +0900

    Update frame_extractor.py

--- frame_extractor.py
+++ frame_extractor.py
@@ -6,17 +6,17 @@ import flow
 from glob import glob
 from joblib import Parallel, delayed
 
-# this code working for Toyota Smarthome dataset
-# https://project.inria.fr/toyotasmarthome/
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--videos-path", type=str, default="./videos/")
     parser.add_argument("--frames-path", type=str, default="./frames/")
     parser.add_argument("--flows-path", type=str, default="./flows/")
-    parser.add_argument("--frame-size", type=int, default=480)
-    parser.add_argument("--quality", type=int, default=0.75)
+    parser.add_argument("--frame-size", type=int, default=360)
+    parser.add_argument("--quality", type=float, default=0.75)
+    parser.add_argument("--error-rate", type=float, default=0.05)
     parser.add_argument("--flow-mode", action="store_true")
     parser.add_argument("--workers", type=int, default=-1)
+    parser.add_argument("--origin-size", action="store_true")
     parser.add_argument("--use-gpu", action="store_true")
     args = parser.parse_args()
 
@@ -31,10 +31,10 @@ if __name__ == "__main__":
     videos_path = glob(os.path.join(args.videos_path, "*"))
 
     if args.flow_mode:
-        arguments = [args.flows_path, args.frame_size, args.quality, 0.05]
+        arguments = [args.flows_path, args.frame_size, args.quality, args.origin_size, args.error_rate]
         extractor = flow.extract
     else:
-        arguments = [args.frames_path, args.frame_size, args.quality]
+        arguments = [args.frames_path, args.frame_size, args.quality, args.origin_size]
         if args.use_gpu:
             extractor = frame.extract_gpu
         else:
