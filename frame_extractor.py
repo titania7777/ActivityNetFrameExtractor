f3b43cdc77a3eeff2c018247a88984a4cdff4ffd
Hyeok Yoon <titania7777@gmail.com>
2020-12-21 23:05:01 +0900

    Update frame_extractor.py

--- frame_extractor.py
+++ frame_extractor.py
@@ -6,17 +6,17 @@ import flow
 from glob import glob
 from joblib import Parallel, delayed
 
-# this code working for ActivityNet dataset
-# http://activity-net.org/index.html
+# this code working for Toyota Smarthome dataset
+# https://project.inria.fr/toyotasmarthome/
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--videos-path", type=str, default="./videos/")
     parser.add_argument("--frames-path", type=str, default="./frames/")
     parser.add_argument("--flows-path", type=str, default="./flows/")
-    parser.add_argument("--frame-size", type=int, default=480) # 480x270, 320x180
-    parser.add_argument("--quality", type=int, default=30)
+    parser.add_argument("--frame-size", type=int, default=480)
+    parser.add_argument("--quality", type=int, default=0.75)
     parser.add_argument("--flow-mode", action="store_true")
-    parser.add_argument("--workers", type=int, default=16)
+    parser.add_argument("--workers", type=int, default=-1)
     parser.add_argument("--use-gpu", action="store_true")
     args = parser.parse_args()
 
@@ -30,23 +30,15 @@ if __name__ == "__main__":
     # get videos path
     videos_path = glob(os.path.join(args.videos_path, "*"))
 
-    # run
     if args.flow_mode:
-        stats = Parallel(
-            n_jobs=args.workers, backend="threading"
-        )(delayed(flow.extract)(
-            [i, len(videos_path)], video_path, args.flows_path, args.frame_size, args.quality) for i, video_path in enumerate(videos_path)
-        )
+        arguments = [args.flows_path, args.frame_size, args.quality, 0.05]
+        extractor = flow.extract
     else:
+        arguments = [args.frames_path, args.frame_size, args.quality]
         if args.use_gpu:
-            stats = Parallel(
-                n_jobs=args.workers, backend="threading"
-            )(delayed(frame.extract_gpu)(
-                [i, len(videos_path)], video_path, args.frames_path, args.frame_size, args.quality) for i, video_path in enumerate(videos_path)
-            )
+            extractor = frame.extract_gpu
         else:
-            stats = Parallel(
-                n_jobs=args.workers, backend="threading"
-            )(delayed(frame.extract_cpu)(
-                [i, len(videos_path)], video_path, args.frames_path, args.frame_size, args.quality) for i, video_path in enumerate(videos_path)
-            )
+            extractor = frame.extract_cpu
+    
+    # run
+    stats = Parallel(n_jobs=args.workers, backend="threading")(delayed(extractor)([i, len(videos_path)], video_path, *arguments) for i, video_path in enumerate(videos_path))
